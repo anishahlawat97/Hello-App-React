91f150c63624c1698b4330b56a16e75877c18e0a
Anish <anish.mi619@gmail.com>
2021-12-22 17:32:42 +0530

    2nd day assignment

---
+++ src/Button.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Button = () => {
+    return(
+        <div className="btn">
+            <button>About Me</button>
+            <button>LinkedIn</button>
+            <button>GitHub</button>
+        </div>
+    )
+}
+
+export default Button;
